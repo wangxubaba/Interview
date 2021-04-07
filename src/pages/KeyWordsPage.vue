fada0c3be91ba70720fd4b5543d7951fd7ce61c1
wangxubaba <wx8339459@163.com>
2021-04-07 15:29:07 +0800

    雷达图、关键词图创建完成 招聘分布数据集成成功

--- src/pages/KeyWordsPage.vue
+++ src/pages/KeyWordsPage.vue
@@ -1,14 +1,62 @@
-<template>
-    <div>
-        This is KeyWordsPage<br>
-        Address: http://localhost:8080/#/key
-    </div>
-</template>
 
+<template>
+  <div class="hello">
+    <div id="app">
+      <wordcloud
+          :data="defaultWords"
+          nameKey="name"
+          valueKey="value"
+          :color="myColors"
+          :showTooltip="false"
+          :wordClick="wordClickHandler">
+      </wordcloud>
+    </div>
+  </div>
+</template>
 <script>
+    import wordcloud from 'vue-wordcloud'
     export default {
-        name: "KeyWordsPage"
+        name: "KeyWordsPage",
+        components: {
+          wordcloud
+        },
+        methods: {
+          wordClickHandler(name, value, vm) {
+            console.log('wordClickHandler', name, value, vm);
+          }
+        },
+      data() {
+        return {
+          myColors: ['#1f77b4', '#629fc9', '#94bedb', '#c9e0ef'],
+          defaultWords: [{
+            "name": "Cat",
+            "value": 26
+          },
+            {
+              "name": "fish",
+              "value": 19
+            },
+            {
+              "name": "things",
+              "value": 18
+            },
+            {
+              "name": "look",
+              "value": 16
+            },
+            {
+              "name": "two",
+              "value": 15
+            },
+            {
+              "name": "fun",
+              "value": 100
+            }
+          ]
+        }
+      }
     }
+
 </script>
 
 <style scoped>
--- src/pages/Radar_1Page.vue
+++ src/pages/Radar_1Page.vue
@@ -1,13 +1,89 @@
 <template>
     <div>
-        This is Radar_1Page<br>
-        Address: http://localhost:8080/#/radar/1
+      <div id="my_chart" style="width: 60%;height:500px;margin-left: 20%">
+      </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Radar_1Page"
+      name: "Radar_1Page",
+      mounted() {
+        this.drawLine();
+      },
+      methods: {
+        drawLine() {
+          // 基于准备好的dom，初始化echarts实例
+          let myChart = this.$echarts.init(document.getElementById('my_chart'))
+          // 绘制图表
+          myChart.setOption({
+            title: {
+              text: '浏览器占比变化',
+              subtext: '纯属虚构',
+              top: 10,
+              left: 10
+            },
+            tooltip: {
+              trigger: 'item'
+            },
+            legend: {
+              type: 'scroll',
+              bottom: 10,
+              data: (function (){
+                var list = [];
+                for (var i = 1; i <=28; i++) {
+                  list.push(i + 2000 + '');
+                }
+                return list;
+              })()
+            },
+            visualMap: {
+              top: 'middle',
+              right: 10,
+              color: ['red', 'yellow'],
+              calculable: true
+            },
+            radar: {
+              indicator: [
+                { text: 'IE8-', max: 400},
+                { text: 'IE9+', max: 400},
+                { text: 'Safari', max: 400},
+                { text: 'Firefox', max: 400},
+                { text: 'Chrome', max: 400}
+              ]
+            },
+            series: (function (){
+              var series = [];
+              for (var i = 1; i <= 28; i++) {
+                series.push({
+                  name: '浏览器（数据纯属虚构）',
+                  type: 'radar',
+                  symbol: 'none',
+                  lineStyle: {
+                    width: 1
+                  },
+                  emphasis: {
+                    areaStyle: {
+                      color: 'rgba(0,250,0,0.3)'
+                    }
+                  },
+                  data: [{
+                    value: [
+                      (40 - i) * 10,
+                      (38 - i) * 4 + 60,
+                      i * 5 + 10,
+                      i * 9,
+                      i * i /2
+                    ],
+                    name: i + 2000 + ''
+                  }]
+                });
+              }
+              return series;
+            })()
+          });
+        }
+      }
     }
 </script>
 
